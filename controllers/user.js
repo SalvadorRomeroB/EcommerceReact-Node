aef35d109869f9cc713a047e7cef369922150f43
Salva Romero <chava0romero@Salvas-MacBook-Pro.local>
2019-11-23 20:15:51 -0700

    backend sign in y sign out implementados

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
@@ -15,4 +17,35 @@ exports.signup = (req, res) => {
       user
     });
   });
+};
+
+exports.signin = (req, res) => {
+  // Encontrar user por email
+  const { email, password } = req.body;
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please sign up"
+      });
+    }
+    // If encontrado check email y password match
+    //check match
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and pasword do not match"
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout succesfull" });
 };
