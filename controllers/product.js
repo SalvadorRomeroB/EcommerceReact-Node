fca52ad3af609d0922380442c6d7c5f28686e139
Salva Romero <chava0romero@Salvas-MacBook-Pro.local>
2019-11-29 12:27:35 -0700

    Products CRUD done

--- controllers/product.js
+++ controllers/product.js
@@ -16,11 +16,13 @@ exports.productById = (req, res, next, id) => {
   });
 };
 
+//Read
 exports.read = (req, res) => {
   req.product.photo = undefined;
   return res.json(req.product);
 };
 
+// Create
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
@@ -51,6 +53,73 @@ exports.create = (req, res) => {
 
     if (files.photo) {
       // Check imagen not too large porque no queremos tronar nada
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image too large, should be less than 1 megabyte"
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+
+      res.json(result);
+    });
+  });
+};
+
+// Delete
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: "Product deleted "
+    });
+  });
+};
+
+// Update
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+      });
+    }
+
+    // Check info is filled
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are needed"
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: "Image too large, should be less than 1 megabyte"
