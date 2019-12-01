f5ce8d8bc97f74cd16e7c0e54dac3fcf6093a1b8
Luis Sanchez <lsjaquez22@gmail.com>
2019-11-30 19:57:39 -0700

    deplot in production

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 const port = process.env.PORT;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
